3c2616a212f49b50a19e84ac5a59c26a606e54f4
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-03-19 15:28:00 +0000

    add + and - functionality

--- script.js
+++ script.js
@@ -2,48 +2,56 @@
 
 const foods = [
   {
+    id: 1,
     name: "Chicken Burger",
     image: "images/burger.png",
     price: 199,
     desc: "Juicy grilled chicken burger with cheese."
   },
   {
+    id: 2,
     name: "Cheese Pizza",
     image: "images/pizza.png",
     price: 299,
     desc: "Fresh oven baked pizza with mozzarella."
   },
   {
+    id: 3,
     name: "Chicken Roll",
     image: "images/chicken-roll.png",
     price: 249,
     desc: "Delicious chicken roll with fresh vegetables."
   },
   {
+    id: 4,
     name: "Veg Sandwich",
     image: "images/sandwich.png",
     price: 149,
     desc: "Healthy grilled sandwich with veggies."
   },
   {
+    id: 5,
     name: "Spring Roll",
     image: "images/spring-roll.png",
     price: 99,
     desc: "Crispy spring rolls filled with veggies."
   },
   {
+    id: 6,
     name: "Spaghetti",
     image: "images/spaghetti.png",
     price: 199,
     desc: "Delicious spaghetti with your choice of sauce."
   },
   {
+    id: 7,
     name: "Lasagna",
     image: "images/lasagna.png",
     price: 249,
     desc: "Delicious layered pasta with cheese and sauce."
   },
   {
+    id: 8,
     name: "Fried Chicken",
     image: "images/fried-chicken.png",
     price: 299,
@@ -82,7 +90,77 @@ foods.forEach(food => {
 
   foodGrid.appendChild(card);
 
+  // click on add to btn 
+  const CartBtn = card.querySelector('.cart-btn')
+  CartBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    addToCart(food);
+  });
+
+
 });
+
+// create empty array for condition check product ID
+const cartProduct = [];
+
+const addToCart = (food) => {
+
+  // existing products 
+  const existingProducts = cartProduct.find(item => item.id === food.id);
+  if (existingProducts) {
+    alert('Item Already In Your Cart!');
+    return;
+  }
+
+  cartProduct.push(food);
+  let quantity = 1;
+  let price = Number(food.price.replace('$', ''))
+
+  const cartItem = document.createElement('div');
+  cartItem.classList.add('item');
+
+  const cartList = document.querySelector('.cart-list');
+
+  cartItem.innerHTML = `
+  <div class="ima-container">
+                        <img src="${food.image}">
+                     </div>
+                     <div class="h4size">
+                        <h4>${food.name}</h4>
+                        <h4 class="total-item">${food.price}</h4>
+                     </div>
+                     <div class="flex">
+                        <a href="#" class="quantity-btn">
+                           <i class="fa-solid fa-minus minus"></i>
+                        </a>
+                        <h4 class="quantity-value">${quantity}</h4>
+                        <a href="#" class="quantity-btn">
+                           <i class="fa-solid fa-plus plus"></i>
+                        </a>
+                     </div>
+  `;
+
+  cartList.appendChild(cartItem);
+
+
+  // Quantity Plus And Minus
+  const plusBtn = cartItem.querySelector('.plus');
+  const quantityValue = cartItem.querySelector('.quantity-value')
+  const itemTotal = cartItem.querySelector('.total-item');
+  plusBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    quantity++
+    quantityValue.textContent = quantity;
+    itemTotal.textContent = '$${price * quantity}'
+
+  })
+
+}
+
+
+
+
 
 
 // hamburger menu toggle
@@ -119,37 +197,37 @@ const prevBtn = document.getElementById("prev");
 
 let index = 0;
 
-function showReview(i){
+function showReview(i) {
 
-reviews.forEach((review)=>{
-review.classList.remove("active");
+  reviews.forEach((review) => {
+    review.classList.remove("active");
+  });
+
+  reviews[i].classList.add("active");
+
+}
+
+nextBtn.addEventListener("click", () => {
+
+  index++;
+
+  if (index >= reviews.length) {
+    index = 0;
+  }
+
+  showReview(index);
+
 });
 
-reviews[i].classList.add("active");
+prevBtn.addEventListener("click", () => {
 
-}
+  index--;
 
-nextBtn.addEventListener("click",()=>{
+  if (index < 0) {
+    index = reviews.length - 1;
+  }
 
-index++;
-
-if(index >= reviews.length){
-index = 0;
-}
-
-showReview(index);
-
-});
-
-prevBtn.addEventListener("click",()=>{
-
-index--;
-
-if(index < 0){
-index = reviews.length - 1;
-}
-
-showReview(index);
+  showReview(index);
 
 });
 
@@ -173,16 +251,16 @@ showReview(index);
 
 const buttons = document.querySelectorAll(".cart-btn");
 
-buttons.forEach(btn=>{
-btn.addEventListener("click",()=>{
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
 
-btn.classList.add("bounce");
+    btn.classList.add("bounce");
 
-setTimeout(()=>{
-btn.classList.remove("bounce");
-},400);
+    setTimeout(() => {
+      btn.classList.remove("bounce");
+    }, 400);
 
-});
+  });
 });
 
 
@@ -193,9 +271,11 @@ const cartTab = document.querySelector(".cart-tab");
 const closeCart = document.getElementById("close-btn")
 
 cartIcon.addEventListener("click", (e) => {
-    e.preventDefault(); // stop page refresh
-    cartTab.classList.toggle("active");
+  e.preventDefault(); // stop page refresh
+  cartTab.classList.toggle("active");
 });
 
 // Close Cart - Tab 
 closeCart.addEventListener("click", () => cartTab.classList.remove("active"));
+
+
