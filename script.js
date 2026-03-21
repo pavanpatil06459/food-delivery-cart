038cc08609793730f9ef4f6aab7f6c5e947ad2b8
pavanpatil06459 <pavanpatil06459@gmail.com>
2026-03-21 23:06:04 +0530

    add fuctionality total item calculation increase decrease quantity of products

--- script.js
+++ script.js
@@ -1,196 +1,229 @@
-// Add Food Items to the Menu
-
+/*FOOD DATA (MENU ITEMS) */
 const foods = [
   {
     id: 1,
     name: "Chicken Burger",
     image: "images/burger.png",
     price: 199,
-    desc: "Juicy grilled chicken burger with cheese."
+    desc: "Juicy grilled chicken burger with cheese.",
   },
   {
     id: 2,
     name: "Cheese Pizza",
     image: "images/pizza.png",
     price: 299,
-    desc: "Fresh oven baked pizza with mozzarella."
+    desc: "Fresh oven baked pizza with mozzarella.",
   },
   {
     id: 3,
     name: "Chicken Roll",
     image: "images/chicken-roll.png",
     price: 249,
-    desc: "Delicious chicken roll with fresh vegetables."
+    desc: "Delicious chicken roll with fresh vegetables.",
   },
   {
     id: 4,
     name: "Veg Sandwich",
     image: "images/sandwich.png",
     price: 149,
-    desc: "Healthy grilled sandwich with veggies."
+    desc: "Healthy grilled sandwich with veggies.",
   },
   {
     id: 5,
     name: "Spring Roll",
     image: "images/spring-roll.png",
     price: 99,
-    desc: "Crispy spring rolls filled with veggies."
+    desc: "Crispy spring rolls filled with veggies.",
   },
   {
     id: 6,
     name: "Spaghetti",
     image: "images/spaghetti.png",
     price: 199,
-    desc: "Delicious spaghetti with your choice of sauce."
+    desc: "Delicious spaghetti with your choice of sauce.",
   },
   {
     id: 7,
     name: "Lasagna",
     image: "images/lasagna.png",
     price: 249,
-    desc: "Delicious layered pasta with cheese and sauce."
+    desc: "Layered pasta with cheese and sauce.",
   },
   {
     id: 8,
     name: "Fried Chicken",
     image: "images/fried-chicken.png",
     price: 299,
-    desc: "Crispy fried chicken with your choice of sauce."
-  }
+    desc: "Crispy fried chicken with sauce.",
+  },
 ];
 
+/*RENDER FOOD CARDS */
 const foodGrid = document.getElementById("foodGrid");
 
-foods.forEach(food => {
-
+foods.forEach((food) => {
   const card = document.createElement("div");
   card.classList.add("food-card");
 
-  card.innerHTML = `  
+  card.innerHTML = `
     <img src="${food.image}" alt="${food.name}">
     
     <div class="food-info">
-
       <h3>${food.name}</h3>
-
       <p class="hide">${food.desc}</p>
 
       <div class="food-bottom">
-
         <span class="price">₹${food.price}</span>
-
-        <button class="cart-btn" data-name="${food.name}">
-          Add to Cart
-        </button>
-
+        <button class="cart-btn">Add to Cart</button>
       </div>
-
     </div>
   `;
 
   foodGrid.appendChild(card);
 
-  // click on add to btn 
-  const CartBtn = card.querySelector('.cart-btn')
-  CartBtn.addEventListener('click', (e) => {
-    e.preventDefault();
+  // Add to Cart button click
+  const cartBtn = card.querySelector(".cart-btn");
+  cartBtn.addEventListener("click", () => {
     addToCart(food);
+
+    // button animation
+    cartBtn.classList.add("bounce");
+    setTimeout(() => cartBtn.classList.remove("bounce"), 400);
   });
-
-
 });
 
-// create empty array for condition check product ID
-const cartProduct = [];
+/* CART LOGIC*/
+let cartProduct = [];
 
 const addToCart = (food) => {
-
-  // existing products 
-  const existingProducts = cartProduct.find(item => item.id === food.id);
-  if (existingProducts) {
-    alert('Item Already In Your Cart!');
+  // Prevent duplicate items
+  const exists = cartProduct.find((item) => item.id === food.id);
+  if (exists) {
+    alert("Item already in cart!");
     return;
   }
 
   cartProduct.push(food);
+
   let quantity = 1;
-  let price = Number(food.price.replace('$', ''))
+  let price = food.price;
 
-  const cartItem = document.createElement('div');
-  cartItem.classList.add('item');
+  const cartItem = document.createElement("div");
+  cartItem.classList.add("item");
 
-  const cartList = document.querySelector('.cart-list');
+  const cartList = document.querySelector(".cart-list");
 
   cartItem.innerHTML = `
-  <div class="ima-container">
-                        <img src="${food.image}">
-                     </div>
-                     <div class="h4size">
-                        <h4>${food.name}</h4>
-                        <h4 class="total-item">${food.price}</h4>
-                     </div>
-                     <div class="flex">
-                        <a href="#" class="quantity-btn">
-                           <i class="fa-solid fa-minus minus"></i>
-                        </a>
-                        <h4 class="quantity-value">${quantity}</h4>
-                        <a href="#" class="quantity-btn">
-                           <i class="fa-solid fa-plus plus"></i>
-                        </a>
-                     </div>
+    <div class="ima-container">
+      <img src="${food.image}">
+    </div>
+
+    <div class="h4size">
+      <h4>${food.name}</h4>
+      <h4 class="total-item">₹${price}</h4>
+    </div>
+
+    <div class="flex">
+      <a href="#" class="quantity-btn">
+        <i class="fa-solid fa-minus minus"></i>
+      </a>
+
+      <h4 class="quantity-value">${quantity}</h4>
+
+      <a href="#" class="quantity-btn">
+        <i class="fa-solid fa-plus plus"></i>
+      </a>
+    </div>
   `;
 
   cartList.appendChild(cartItem);
+  updateTotals();
+
+  /* Quantity Controls*/
+  const plusBtn = cartItem.querySelector(".plus");
+  const minusBtn = cartItem.querySelector(".minus");
+  const quantityValue = cartItem.querySelector(".quantity-value");
+  const itemTotal = cartItem.querySelector(".total-item");
+  const cartTotal = document.querySelector('.cart-total');
+
+  // Increase quantity
+  plusBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    quantity++;
+    quantityValue.textContent = quantity;
+    itemTotal.textContent = `₹${price * quantity}`;
+    updateTotals();
+  });
 
 
-  // Quantity Plus And Minus
-  const plusBtn = cartItem.querySelector('.plus');
-  const quantityValue = cartItem.querySelector('.quantity-value')
-  const itemTotal = cartItem.querySelector('.total-item');
-  plusBtn.addEventListener('click', (e) => {
+
+  // Decrease quantity
+  minusBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
-    quantity++
-    quantityValue.textContent = quantity;
-    itemTotal.textContent = '$${price * quantity}'
+    if (quantity > 1) {
+      quantity--;
+      quantityValue.textContent = quantity;
+      itemTotal.textContent = `₹${price * quantity}`;
+       updateTotals();
+    } else {
+      cartItem.classList.add("slide-out");
 
-  })
+      setTimeout(() => {
+        cartItem.remove();
+        cartProduct = cartProduct.filter((item) => item.id !== food.id);
+        updateTotals();
+      }, 300);
+    }
+    
+  });
+};
 
-}
+const cartTotal = document.querySelector('.cart-total');
+const cartValue = document.querySelector('.cart-value');
+
+const updateTotals = () => {
+  let totalPrice = 0;
+  let totalQuantity = 0;
+
+  document.querySelectorAll('.item').forEach(item => {
+
+    const quantity = Number(item.querySelector('.quantity-value').textContent);
+
+    const priceText = item.querySelector('.total-item').textContent;
+    const price = parseInt(priceText.replace('₹', ''));
+
+    totalPrice += price;
+    totalQuantity += quantity;
+  });
+
+  cartTotal.textContent = `₹${totalPrice}`;
+  cartValue.textContent = totalQuantity;
+};
 
 
 
-
-
-
-// hamburger menu toggle
-
+/* HAMBURGER MENU*/
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 hamburger.addEventListener("click", () => {
-
-  mobileMenu.classList.toggle("active");
   hamburger.classList.toggle("active");
-
+  mobileMenu.classList.toggle("active");
 });
 
 
-// close menu when clicking outside
-
+// Close menu when clicking outside
 document.addEventListener("click", (e) => {
-
   if (!hamburger.contains(e.target) && !mobileMenu.contains(e.target)) {
-
-    mobileMenu.classList.remove("active");
     hamburger.classList.remove("active");
-
+    mobileMenu.classList.remove("active");
   }
-
 });
 
 
-//Slider for reviews
+
+/*  REVIEW SLIDER */
 const reviews = document.querySelectorAll(".review");
 const nextBtn = document.getElementById("next");
 const prevBtn = document.getElementById("prev");
@@ -198,84 +231,41 @@ const prevBtn = document.getElementById("prev");
 let index = 0;
 
 function showReview(i) {
-
-  reviews.forEach((review) => {
-    review.classList.remove("active");
-  });
-
+  reviews.forEach((r) => r.classList.remove("active"));
   reviews[i].classList.add("active");
-
 }
 
+
+// Next
 nextBtn.addEventListener("click", () => {
-
-  index++;
-
-  if (index >= reviews.length) {
-    index = 0;
-  }
-
+  index = (index + 1) % reviews.length;
   showReview(index);
-
 });
 
+
+
+// Previous
 prevBtn.addEventListener("click", () => {
-
-  index--;
-
-  if (index < 0) {
-    index = reviews.length - 1;
-  }
-
+  index = (index - 1 + reviews.length) % reviews.length;
   showReview(index);
-
 });
 
-
-// AUTO SLIDE
-
-// setInterval(()=>{
-
-// index++;
-
-// if(index >= reviews.length){
-// index = 0;
-// }
-
-// showReview(index);
-
-// },3000);
-
-
-// bounce effct for add to cart button
-
-const buttons = document.querySelectorAll(".cart-btn");
-
-buttons.forEach(btn => {
-  btn.addEventListener("click", () => {
-
-    btn.classList.add("bounce");
-
-    setTimeout(() => {
-      btn.classList.remove("bounce");
-    }, 400);
-
-  });
-});
-
-
-
-// This is for show cart on window (toggle)
+/* CART TOGGLE*/
 const cartIcon = document.querySelector(".cart-icon");
 const cartTab = document.querySelector(".cart-tab");
-const closeCart = document.getElementById("close-btn")
+const closeCart = document.getElementById("close-btn");
 
+
+
+// Open/Close cart
 cartIcon.addEventListener("click", (e) => {
-  e.preventDefault(); // stop page refresh
+  e.preventDefault();
   cartTab.classList.toggle("active");
 });
 
-// Close Cart - Tab 
-closeCart.addEventListener("click", () => cartTab.classList.remove("active"));
 
 
+// Close button
+closeCart.addEventListener("click", () => {
+  cartTab.classList.remove("active");
+});
